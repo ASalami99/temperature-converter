9976efea2b52fbb010e9a44d1f90e692fa38bea1
ASalami99 <115806577+ASalami99@users.noreply.github.com>
2024-10-29 22:39:34 +0100

    Basic_Layout

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import { s } from "./App.style";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { Text } from "react-native";
+import { Text, View, ImageBackground } from "react-native";
+import hotBackground from "./assets/hot.png";
 
 export default function App() {
   return (
+    <ImageBackground style = {s.backgroundimage}source={hotBackground}>
     <SafeAreaProvider>
       <SafeAreaView style={s.root}>
-        <Text>Hello!</Text>
+        <View style={s.workspace}>
+          <Text>Temperature</Text>
+          <Text>Input</Text>
+          <Text>Button</Text>
+        </View>
       </SafeAreaView>
     </SafeAreaProvider>
+    </ImageBackground>
   );
 }
 
--- App.style.js
+++ App.style.js
@@ -3,5 +3,17 @@ import { StyleSheet } from "react-native";
 export const s = StyleSheet.create({
     root: {
         flex: 1,    //We have the whole space (page) available to work in
+        justifyContent: "center"
+    },
+
+    workspace: {
+        height: 450,
+        //backgroundColor: "red", // We don't need this again. It was just to show where the workspace was\
+        alignItems: "center",
+        justifyContent: "space-evenly"
+    },
+
+    backgroundimage: {
+        height: "100%"
     }
 })
